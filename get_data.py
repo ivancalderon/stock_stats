6d399282394ea65b4a9e076dac2f9138c77c30ee
ivancalderon <ivancalderonrodriguez@gmail.com>
2020-03-26 10:22:40 -0500

    Add files via upload

---
+++ get_data.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 09:49:17 2019
+
+@author: ivancalderon
+"""
+
+# %%
+import requests
+import pandas as pd
+import subprocess
+import os
+import datetime as dt
+import numpy as np
+import asyncio
+
+
+# %%
+def get_data_path():
+    """
+    Get path to alphavantage.co data API
+    by decryptinig key file, using a customized
+    bash script\n
+    timeframe: string, two options 'daily' or 'intraday'
+    """
+    wd = "/Users/ivancalderon/Documents/Trading/Context"
+    if not os.getcwd() == wd:
+        os.chdir(wd)
+    command = "/usr/local/bin/decriptsource.sh"
+    encripted = "encrypsource.txt"
+    output = "temp.txt"
+    password = "password.txt"
+    args = [command, encripted, output, password]
+    if os.path.isfile(output):
+        os.remove(output)
+    subprocess.run(args, stdout=subprocess.PIPE)
+    with open(output, 'r') as text:
+        source = text.read().rstrip("\n")
+    os.remove(output)
+    a = "symbol={}&outputsize"
+    b = "symbol={}&interval={}&outputsize"
+    return {'intraday': source.replace(a, b), 'daily': source}
+
+
+path_dict = get_data_path()
+# %%
+
+
+def pull_data(timeframe, stock, size, freq=None):
+    """
+    Pull price action data from alphavantage.co\n
+    timeframe: string, two options 'daily' or 'intraday'\n
+    stock: string, ticker of the stock\n
+    size: string, two options 'full' or 'compact'\n
+    freq: int, type of minute chart. One of 1, 5, 15, 30, 60\n
+    """
+    functions = {"daily": "TIME_SERIES_DAILY",
+                 "intraday": "TIME_SERIES_INTRADAY"
+                 }
+    path = path_dict[timeframe]
+    print(f'Path {timeframe} served from dictionary')
+    if timeframe == "intraday":
+        if freq is None:
+            raise ValueError("If timeframe is intraday, freq must be given")
+        else:
+            interval = f'{freq}min'
+            path = path.format(functions[timeframe],
+                               stock.upper(),
+                               interval,
+                               size)
+    elif timeframe == "daily":
+        path = path.format(functions[timeframe],
+                           stock.upper(),
+                           size)
+    data = requests.get(path).json()
+    series = list(data.keys())[1]
+    df = pd.DataFrame.from_dict(data[series],
+                                orient="index"
+                                )
+    df.columns = ["Open", "High", "Low", "Close", "Volume"]
+    df = df.astype({"Open": float,
+                    "High": float,
+                    "Low": float,
+                    "Close": float,
+                    "Volume": float
+                    },
+                   copy=False
+                   )
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+# %%
+async def pull_async(timeframe, stock, size, freq=None):
+    await asyncio.sleep(0)
+    mydf = pull_data(timeframe, stock, size, freq)
+    return mydf
+
+
+def task_manager(ticker):
+    ticker = ticker.upper()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    tasks = [
+            asyncio.ensure_future(pull_async("intraday", ticker, "full", 15)),
+            asyncio.ensure_future(pull_async("daily", ticker, "compact"))
+            ]
+    try:
+        loop.run_until_complete(asyncio.gather(*tasks))
+    except RuntimeError:
+        pass
+    loop.close()
+    return {"intraday": tasks[0],
+            "daily": tasks[1]}
+
+
+# %%
+def clear_repeated(array):
+    if 1 not in array:
+        return array
+    case1 = np.array([0, 1, 1])
+    case2 = np.array([0, 0, 1])
+    case3 = np.array([0, 0, 0])
+    pointer = {0: case1,
+               1: case2,
+               2: case3
+               }
+    position = list(array).index(1)
+    x = array - pointer[position]
+    return x
+
+
+# %%
+def test_time_range(x):
+    fifteen = dt.time(9, 45)
+    half = dt.time(10, 00)
+    hour = dt.time(10, 30)
+    compare = np.array([fifteen,
+                        half,
+                        hour
+                        ]
+                       )
+    # use *1 to convert array of bools to int
+    y = (x.time() <= compare)*1
+    return clear_repeated(y)
+
+
+# %%
+def expected_move(df, timeframe):
+    if timeframe not in ["daily", "intraday"]:
+        raise ValueError("timeframe not accepted")
+    if timeframe == "intraday":
+        point = dt.time(10, 30)
+        move = df[df.index.time <= point].copy()
+    elif timeframe == "daily":
+        move = df
+    move.loc[:, "Range"] = move["High"] - move["Low"]
+    return move
+
+
+# %%
+def first_hour_peak(df):
+    """
+    df : intraday(15min) dataframe with stock price\n
+    return:
+        times highs or lows happen in the 1st hour
+    """
+    y = df.groupby(pd.Grouper(freq="B"))
+    hole = []
+    for day in y:
+        x = day[1]
+        try:
+            hole.append(x.idxmax())
+        except ValueError:
+            pass
+    m = pd.DataFrame(hole)[["High", "Low"]]
+    h = m["High"].apply(test_time_range).sum() / m["High"].count() * 100
+    l = m["Low"].apply(test_time_range).sum() / m['Low'].count() * 100
+    return (h, l)
+
+
+# %%
+def week_day_move(df):
+    """
+    df: daily timeframe stock prices.\n
+    return:
+        range by day of the week
+    """
+    df.loc[:, 'week_day'] = df.index.day_name()
+    df.loc[:, "Range"] = df["High"] - df["Low"]
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    data = {x: df.loc[df['week_day']== x] for x in days}
+    return data
---
+++ stock_stats_app.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 02 10:13:22 2019
+
+@author: ivancalderon
+"""
+# %%
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input
+from dash.dependencies import Output
+import get_data as gt
+import plotly.graph_objs as go
+import pandas as pd
+import datetime as dt
+import json
+import numpy as np
+# %%
+
+
+path = '/Users/ivancalderon/Documents/Trading/Record/low_spread_stocks.txt'
+with open(path, 'r') as text:
+    ticker_list = text.read().rsplit('\n')
+options = []
+for tick in sorted(ticker_list):
+    options.append({'label': tick, 'value': tick})
+# %%
+app = dash.Dash(__name__)
+app.layout = html.Div(children=[
+    html.H1('Stock Price Statistics', className='title'),
+    html.Div(className='mini-container',
+            children=[
+                    dcc.Dropdown(
+                                id='ticker_dropdown',
+                                options=options,
+                                value='x',
+                                style={'width': '300px'}
+                                 ),
+                    html.H3('number', id='last_price')
+                     ]
+             ),
+    html.Div(className='grid-container',
+             children=[
+             dcc.Graph(
+                id='expec_by_hour',
+                className='item1',
+                config={'displayModeBar': False}
+                ),
+             dcc.Graph(
+                id='highs_1st_hour',
+                className='item2',
+                config={'displayModeBar': False}
+            ),
+             dcc.Graph(
+                id='lows_1st_hour',
+                className='item3',
+                config={'displayModeBar': False}
+            ),
+             dcc.Graph(
+                id='day_of_week',
+                className='item4',
+                config={'displayModeBar': False}
+            ),
+            dcc.Graph(
+                id='volume_dist',
+                className='item6',
+                config={'displayModeBar': False}
+            ),
+            dcc.Graph(
+                id='daily_range',
+                className='item5',
+                config={'displayModeBar': False}
+            ),
+            dcc.Graph(
+                id='tvol',
+                className='item7',
+                config={'displayModeBar': False}
+            ),
+            html.Div(
+                id="data_store",
+                style={'display': 'none'}
+            )
+             ])
+    ])
+
+@app.callback(
+    Output('data_store', 'children'), 
+    [Input('ticker_dropdown', 'value')]
+    )
+def clean_data(value):
+   start = dt.datetime.now()
+   futures =  gt.task_manager(value)
+   intraday = futures['intraday'].result()
+   daily = futures['daily'].result()
+   data_dict = {"intraday": intraday.to_json(),
+            "daily": daily.to_json()}
+   print(dt.datetime.now() - start)
+   return json.dumps(data_dict)
+
+
+@app.callback(
+    Output('last_price', 'children'), 
+    [Input('data_store', 'children')]
+    )
+def get_last_price(data_dict):
+    data = json.loads(data_dict)
+    values = pd.read_json(data['intraday'])
+    o = float(values['Open'].tail(1))
+    h = float(values['High'].tail(1))
+    l = float(values['Low'].tail(1))
+    c = float(values['Close'].tail(1))
+    v = float(values['Volume'].tail(1))/1000000
+    nice = f"Last Print... Open: ${o:,.2f}, High: ${h:,.2f}, Low: ${l:,.2f}, Close: ${c:,.2f}, Volume: {v:,.2f}"
+    return nice
+
+
+
+@app.callback(
+    Output('expec_by_hour', 'figure'), 
+    [Input('data_store', 'children')]
+    )
+def update_expec_hour(data_dict):
+    data = json.loads(data_dict)
+    values = gt.expected_move(pd.read_json(data['intraday']), 'intraday')
+    points = [('9:30', '9:45'), ('9:45', '10:00'), ('10:00', '10:15'), ('10:15', '10:30')]
+    traces = []
+    for point in points:
+        start, end = point
+        df = values.between_time(start, end, include_start=False)
+        traces.append(go.Box(y=df['Range'],
+                             showlegend=False,
+                             name=end)
+                      )
+    return {
+        'data': traces,
+        'layout': go.Layout(title='Expected move each 15 min',
+                            legend=None,
+                            autosize=False,
+                            margin={'l': 30, 'r': 5, 't': 65, 'b': 40},
+                            #height=250,
+                            #width=300
+                            )
+    }
+
+
+@app.callback(
+    [Output('highs_1st_hour', 'figure'),
+     Output('lows_1st_hour', 'figure'),
+     Output('tvol', 'figure')], 
+    [Input('data_store', 'children')]
+    )
+def update_peaks(data_dict):
+    data = json.loads(data_dict)
+    intraday = pd.read_json(data['intraday'])
+    h, l = gt.first_hour_peak(intraday)
+    y = intraday.groupby(pd.Grouper(freq="B"))
+    traces = []
+    for day in y:
+        df = day[1].copy()
+        df.loc[:, 'volcumsum'] = (df.loc[:, 'Volume']
+                                    .cumsum()
+                                    .shift(1))
+        traces.append(go.Bar(x=df.index, y=df['Volume'],
+                base=df['volcumsum'],
+                marker_color='blue',
+                showlegend=False
+                ))
+    tvol = {'data': traces[-7:],
+            'layout': go.Layout(
+                                title='Total Volume each 15 min',
+                                xaxis={'type': 'category',
+                                       #'nticks': 7,
+                                       'tickangle': -45,
+                                       'tickformat': '%d-%m',
+                                       #'tickmode': 'auto',
+                                       'tickvals': [],
+                                       'ticktext': []
+                                       }
+                                    
+            ) }
+    h_figure = {'data': [go.Bar(y=h, marker={'color': 'blue'})],
+                'layout': go.Layout(
+                    title='% times that Highs occured in interval',
+                    legend=None,
+                    margin={'l': 30, 'r': 5, 't': 65, 'b': 40},
+                    xaxis={
+                        'ticktext': ['15min', '30min', '1h'],
+                        'tickvals': list(range(3))
+                    },
+                    yaxis={'range': (0, 100)},
+                    #height=250,
+                    #width=300
+                )}
+    l_figure = {'data': [go.Bar(y=l, marker={'color': 'orange'})],
+                'layout': go.Layout(
+                    title='% times that Lows occured in interval',
+                    legend=None,
+                    margin={'l': 30, 'r': 5, 't': 65, 'b': 40},
+                    xaxis={
+                        'ticktext': ['15min', '30min', '1h'],
+                        'tickvals': list(range(3))
+                    },
+                    yaxis={'range': (0, 100)},
+                    #height=250,
+                    #width=300
+                )}
+    return h_figure, l_figure, tvol
+
+
+@app.callback(
+    [Output('day_of_week', 'figure'),
+     Output('daily_range', 'figure'),
+     Output('volume_dist', 'figure')], 
+    [Input('data_store', 'children')]
+    )
+def update_day_of_week(data_dict):
+    data = json.loads(data_dict)
+    values = gt.week_day_move(pd.read_json(data['daily']))
+    traces = []
+    vol_traces = []
+    for x in values.keys():
+        info = values[x]
+        traces.append(go.Box(y=info['Range'],
+                             name=x,
+                             showlegend=False))
+        vol_traces.append(go.Box(y=info['Volume'],
+                             name=x,
+                             showlegend=False))
+    day_spread = {'data': traces,
+           'layout': go.Layout(
+                                title='Range spread per day of the week',
+                                #height=250,
+                                #width=450
+                                )
+           }
+    vol_dist = {'data': vol_traces,
+           'layout': go.Layout(
+                                title='Volume spread per day of the week',
+                                #height=250,
+                                #width=450
+                                )
+           }
+    df = pd.read_json(data['daily'])
+    df.loc[:, 'sign'] = np.where(df['Close'] > df['Open'], 1, -1)
+    df.loc[:, 'Range2'] = (df['High'] - df['Low']) * df['sign']
+    colors = ["green" if x > 0 else "red" for x in df['Range2'].tail(30).values]
+    dates = [dt.datetime.strftime(x, "%d-%m") for x in df['Range2'].tail(30).index]
+    day_range = {'data': [go.Bar(y=df["Range2"].tail(30), marker={'color': colors})],
+            'layout': go.Layout(
+                                title='Range of Stock in last 30 days',
+                                xaxis={
+                                'ticktext': dates,
+                                'tickvals': list(range(len(df['Range2'].tail(30)))),
+                                'tickfont': {'size': 14}},
+                                #height=250,
+                                #width=900
+                                )
+            }
+    return day_spread, day_range, vol_dist
+
+# %%
+if __name__ == '__main__':
+    app.run_server(debug=True, dev_tools_ui=False)
